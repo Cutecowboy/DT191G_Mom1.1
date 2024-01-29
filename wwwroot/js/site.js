39d689ee4005c87b4d3bd2abbb10754e47da1591
Cutecowboy <110782320+Cutecowboy@users.noreply.github.com>
2024-01-29 13:02:30 +0100

    structured design via bootstrap, setup the pages content and fixed js functionality to highlight which page is active togheter with CSS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+let url = window.location.pathname;
+
+if (url == "/") {
+    document.getElementById("index").classList.add("active");
+
+} else if (url == "/Razor") {
+    document.getElementById("razor").classList.add("active");
+} else if (url == "/About") {
+    document.getElementById("about").classList.add("active");
+}
